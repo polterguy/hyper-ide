06a4fe666a194b365ee86193635ab5b6cffc6aaf
Thomas Hansen <phosphorusfive@gmail.com>
2017-12-20 19:08:17 +0200

    Testing

--- helpers/shell-snippets/git-commit.sh
+++ helpers/shell-snippets/git-commit.sh
@@ -1,7 +1,5 @@
-# Exchange COMMIT MESSAGE to whatever you want it to be
+# Add a custom COMMIT MESSAGE below
 VAR1="$(git add .)"
 VAR2="$(git commit -a -m 'COMMIT MESSAGE')"
-VAR3="$(git push --all)"
 echo "${VAR1}"
 echo "${VAR2}"
-echo "${VAR3}"
