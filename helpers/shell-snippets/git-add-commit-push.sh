78a816a7e9af8449d2f1c68e5d9a1868e30b985c
Thomas Hansen <phosphorusfive@gmail.com>
2017-12-20 23:20:17 +0200

    Added convenience script

---
+++ helpers/shell-snippets/git-add-commit-push.sh
@@ -0,0 +1,11 @@
+# NOTICE, Please supply a friendly commit message below.
+
+# This will will add all changes
+git add -A .
+
+# Commits your changes
+git commit -a -m "YOUR COMMIT MESSAGE GOES HERE"
+
+# This will will push all changes to your main repo.
+git push --all
+
