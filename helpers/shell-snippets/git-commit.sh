763828954727deba8fc91e446218b44d1d18507b
Thomas Hansen <phosphorusfive@gmail.com>
2017-12-20 19:38:18 +0200

    COMMIT MESSAGE

--- helpers/shell-snippets/git-commit.sh
+++ helpers/shell-snippets/git-commit.sh
@@ -1,8 +1,9 @@
 # This will will add all changes to your existing files, and commit
-# them to your local repository
+# them to your local repository.
 #
 # Notice, it WILL NOT PUSH your changes to the remote repo.
-# Add a COMMIT MESSAGE below
+#
+# Add a COMMIT MESSAGE below.
 VAR1="$(git add .)"
 VAR2="$(git commit -a -m 'COMMIT MESSAGE')"
 echo "${VAR1}"
